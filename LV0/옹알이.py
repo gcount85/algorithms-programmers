fbd37670135948cc937b0d436dd5fa8e2210a60f
gcount85 <100mgml@gmail.com>
2022-10-20 19:50:23 +0900

    lv0 옹알이 87.5점

--- LV0/옹알이.py
+++ LV0/옹알이.py
@@ -1,24 +1,25 @@
+babbling = ["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]	
+발음 = ["aya", "ye", "woo", "ma"]
+
 def solution(babbling):
-    발음 = ["aya", "ye", "woo", "ma"]
-    가능한발음 = [i+j for i in 발음 for j in 발음 if i != j]
-    print(가능한발음)
     c = 0
-    # for i in 발음:
-    #     if i in babbling:            
-    #         c += 1
-    # for i in 가능한발음:
-    #     if i in babbling:
-    #         c += 1
-            
     for i in babbling:
         for j in 발음:
             if i == j:
                 c += 1
             elif i.startswith(j) and i != j:
                 for k in 발음:
-                    if k != j:
-                        if i == (j+k):
-                            c += 1
-    
+                    if k != j and i == (j+k):
+                        c += 1
+                    elif i.startswith(j+k) and i != (j+k):
+                        for l in 발음:
+                            if l != k and i == (j+k+l):
+                                c += 1  
+                            elif i.startswith(j+k+l) and i != (j+k+l):
+                                for m in 발음:
+                                    if m != l and i == (j+k+l+m):
+                                        c += 1
     
     return c
+
+print(solution(babbling))
