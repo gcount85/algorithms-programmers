02b42531eca89d3f00a42c964196b382c6024bcc
gcount85 <100mgml@gmail.com>
2022-10-21 14:57:01 +0900

    lv0 평행 통과

--- LV0/평행.py
+++ LV0/평행.py
@@ -1,13 +1,13 @@
 #          0      1        2        3
-dots = 	[[3, 5], [4, 1], [2, 4], [5, 10]]
+dots = 	[[3, 5], [3, 1], [2, 4], [5, 10]]
 
 def solution(dots):
     lst = []
     for i in range(0,3):
         for j in range(1,4):
-            if j > i:
+            # 제로디비전 처리를 안 해주면 런타임 에러 뜸 
+            if j > i and (dots[j][0]-dots[i][0]) != 0:  
                 grad = (dots[j][1]-dots[i][1])/(dots[j][0]-dots[i][0])
-                print(grad, lst)
                 if grad in lst:
                     return 1
                 lst.append(grad)
