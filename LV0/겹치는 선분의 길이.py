aa78e82c9ea7d8097f7c5e291325b62f45a0d232
gcount85 <100mgml@gmail.com>
2022-10-21 16:57:33 +0900

    lv0 겹치는 선분의 길이 42.9점

--- LV0/겹치는 선분의 길이.py
+++ LV0/겹치는 선분의 길이.py
@@ -1,16 +1,18 @@
 #          0       1       2
-lines = [[0, 5], [3, 9], [1, 10]]	
+lines = [[0, 1], [2, 5], [3, 9]]	
 def solution(lines):
     차이 = 0
-    lines.sort()
-    # [[0, 5], [1, 10], [3, 9]]
+    lines.sort(key=lambda x: (x[0], x[1]))
+    print(lines)
     for i in range(2):
-        if lines[i][1] < lines[i+1][1]:
-            if (lines[i][1]-lines[i+1][0]) > 0:
+        if lines[i][1] <= lines[i+1][0]:
+            continue
+        if lines[i][1] > lines[i+1][0]:
+            if lines[i][1] > lines[i+1][1]:
+                차이 += lines[i+1][1]-lines[i+1][0]
+            else:
                 차이 += lines[i][1]-lines[i+1][0]
-        if lines[i][1] > lines[i+1][1]:
-            차이 += lines[i+1][1]-lines[i+1][0]
-        if i == 0 and lines[i][1] > lines[i+2][0]:
+        if i == 0 and lines[i][1] >= lines[i+2][0]:
             차이 -= lines[i][1]-lines[i+2][0]
 
     return 차이
