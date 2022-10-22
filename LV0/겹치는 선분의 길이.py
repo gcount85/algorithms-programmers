f39b9661097798067cf9e4e6360a039280d707e3
gcount85 <100mgml@gmail.com>
2022-10-23 00:30:38 +0900

    lv0 겹치는 선분의 길이 85.7점

--- LV0/겹치는 선분의 길이.py
+++ LV0/겹치는 선분의 길이.py
@@ -1,25 +1,36 @@
 #          0       1       2
-lines = [[0, 5], [1, 10], [3, 9]]
+lines = [[0, 5], [3, 9], [1, 10]]
+
 def solution(lines):
     차이 = 0
+    [i.sort() for i in lines]
     lines.sort(key=lambda x: (x[0], x[1]))
+    a = lines[0][0]
+    b = lines[1][0]
+    c = lines[2][0]
+    x = lines[0][1]
+    y = lines[1][1]
+    z = lines[2][1]
     print(lines)
-    for i in range(2):
-        if lines[i][1] <= lines[i+1][0]:
-            continue
-        elif lines[i][1] > lines[i+1][0]:
-            if lines[i][1] > lines[i+1][1]:
-                차이 += lines[i+1][1]-lines[i+1][0]
-            else:
-                차이 += lines[i][1]-lines[i+1][0]
-    if lines[0][1] > lines[2][0]:
-        if lines[0][1] > lines[2][1]:
-            차이 += lines[2][1]-lines[2][0]
-        elif lines[1][1] >= lines[0][1]:
-            차이 -= lines[0][1]-lines[2][0]
-        else:
-            차이 += lines[0][1]-lines[2][0]
-        
+    if a == b == c:
+        차이 += y - a
+    elif a < b == c:
+        차이 += y - b
+    elif a == b < c:
+        차이 += x - a
+        if y > c:
+            차이 += y - c
+        if x > c:
+            차이 -= (x - c)
+    elif a < b < c:
+        if x > b: 
+            차이 += x - b
+        if z < y:
+            차이 += z - c
+        if z > y:
+            차이 += y - c
+        if x > c:
+            차이 -= (x - c)
     return 차이
 
 print(solution(lines))
